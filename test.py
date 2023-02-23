b80fba8432668f1309393e53e542e5c5e38aa8ac
Yunzhi Gao <gaoyunzhi@gmail.com>
2023-02-23 08:31:47 -0800

    commit

--- test.py
+++ test.py
@@ -12,12 +12,12 @@ with open('credential') as f:
 tele = Updater(credential['bot_token'], use_context=True)
 chat = tele.bot.get_chat(credential['debug_group'])
 
-def testImp(status):
+def testSend(status):
     result = mastodon_2_album.get(status)
     print(result)
     album_sender.send_v2(chat, result)
 
-def test():
+def testAllStatus():
     mastodon = Mastodon(
         access_token = 'db/main_mastodon_secret',
         api_base_url = credential['mastodon_domain']
@@ -36,15 +36,18 @@ def testFindAccount(text):
     )
     print(mastodon_2_album.findAccount(mastodon, text))
 
-def testStatusGet(status_search_key):
+def testLog(status):
+    print(mastodon_2_album.getLog(status))
+
+def test(status_search_key):
     mastodon = Mastodon(
         access_token = 'db/main_mastodon_secret',
         api_base_url = credential['mastodon_domain']
     )
     status = mastodon.search_v2(status_search_key)['statuses'][0]
-    testImp(status)
+    testLog(status)
     
 if __name__=='__main__':
-    # test()
+    # testAllStatus()
     # testFindAccount('79470')
-    testStatusGet('https://bgme.me/@ShrimpZhou/107432772239009987')
+    test('https://bgme.me/@all100pure/109419471776444523')
